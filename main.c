bb5c298bd4b7bf93846cecc84526999a4fb1d4c7
Iori YONEJI <fivo.11235813@gmail.com>
2017-12-01 23:32:02 +0900

    add process stack

--- main.c
+++ main.c
@@ -4,14 +4,15 @@
 #include"lpc11xx_iocon.h"
 
 void (* const vectors[])(void) __attribute__ ((section(".vectors"))) ;
+#define SYSAHBCLKCTRL (* (volatile unsigned long *) (APB_START + 0x48080))
 
 int
 main () {
+	volatile uint32_t count;
+	SYSAHBCLKCTRL |= (1 << 16);
 	GPIO1DIR = 1 << 8;
-	while(1) {
-		for (int i = 0x200000; i; i --) {
-		}
-		GPIO1DATA = 0;
-	}
+	GPIO1DATA = 0;
+	for (count = 0; count < 1000000; count ++) ;
+	GPIO1DATA ^= 1 << 8;
 }
 
